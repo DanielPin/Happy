858f49c2d5653b4c76bd2e91088b6b5e9efe9eb5
Daniel Pinheiro Nascimento <daniel.pin@outlook.com>
2020-10-21 01:08:19 -0300

    reta final

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -5,9 +5,12 @@ const options = {
     scrollWheelZoom: false,
     zoomControl: false
 }
+// Pegar valores do html 
+const lat = document.querySelector('span[data-lat]').dataset.lat;
+const lng = document.querySelector('span[data-lng]').dataset.lng;
 
 //create map
-const map = L.map('mapid', options).setView([-23.4954397,-46.6365211], 15);
+const map = L.map('mapid', options).setView([lat,lng], 15);
 
 // create and add tilelayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
@@ -22,8 +25,7 @@ const icon = L.icon({
 })
 
 //create and add marker
-
-L.marker([-23.4954397,-46.6365211], { icon })
+L.marker([lat,lng], { icon })
     .addTo(map)
 
 
--- src/database/fakedata.js
+++ src/database/fakedata.js
@@ -33,5 +33,20 @@ module.exports =[
         open_on_weekends: "0"
     },
 
+    {
+        id: 3,
+        lat: "-23.4381929",
+        lng: "-46.7059374",
+        name: "Lar da Amanda",
+        description:  "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images:[
+            "https://images.unsplash.com/photo-1570570626315-95c19358f053?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1595207011175-3d72f5a3b21c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar." ,
+        opening_hours: "Horário de visitas das 18h até 8h ",
+        open_on_weekends: "0"
+    },
+
 
 ]
